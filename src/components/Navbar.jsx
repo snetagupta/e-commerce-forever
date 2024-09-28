b6cc12027acf95c638106a9c16c9c5c9335d2414
sneta gupta <snetagupta4nov@gmail.com>
2024-09-28 21:05:07 +0530

    Added filters

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import { NavLink, Link } from "react-router-dom";
 import { assets } from "../assets/assets";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
 
 const Navbar = () => {
     const[visible,setVisible] = useState(false)
+    const{setShowSearch} = useContext(ShopContext)
   return (
     <div className="flex items-center justify-between py-5 font-medium">
      <Link to='/'> <img src={assets.logo} className="w-36" alt="" /></Link>
@@ -36,7 +38,7 @@ const Navbar = () => {
       </ul>
 
       <div className="flex items-center gap-6">
-        <img src={assets.search_icon} className="w-5 cursor-pointer" alt="" />
+        <img onClick={()=>setShowSearch(true)} src={assets.search_icon} className="w-5 cursor-pointer" alt="" />
         <div className="group relative">
           <img
             src={assets.profile_icon}
