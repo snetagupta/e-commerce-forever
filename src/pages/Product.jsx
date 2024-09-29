b22e4efcb68ba38b64fe3f44439534b25c9403d0
sneta gupta <snetagupta4nov@gmail.com>
2024-09-29 11:49:26 +0530

    added cart functionality

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,20 +1,21 @@
 import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
+import { assets } from "../assets/assets";
+import RelatedProducts from "../components/RelatedProducts";
 
 const Product = () => {
   const { productId } = useParams();
-  const { products } = useContext(ShopContext);
+  const { products, currency,addToCart} = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
   const [image, setImage] = useState("");
+  const [size, setSize] = useState("");
 
   const fetchProductData = async () => {
     products.map((item) => {
       if (item._id === productId) {
         setProductData(item);
         setImage(item.image[0]);
-        console.log(item);
-
         return null;
       }
     });
@@ -46,8 +47,75 @@ const Product = () => {
           </div>
         </div>
         {/* Product Info */}
-        <div></div>
+        <div className="flex-1">
+          <h1 className="font-medium text-2xl mt-2">{productData.name}</h1>
+          <div className="flex items-center gap-1 mt-2">
+            <img className="w-3 5" src={assets.star_icon} alt="" />
+            <img className="w-3 5" src={assets.star_icon} alt="" />
+            <img className="w-3 5" src={assets.star_icon} alt="" />
+            <img className="w-3 5" src={assets.star_icon} alt="" />
+            <img className="w-3 5" src={assets.star_dull_icon} alt="" />
+            <p className="pl-2">(122)</p>
+          </div>
+          <p className="mt-5 text-3xl font-medium">
+            {currency} {productData.price}
+          </p>
+          <p className="mt-5 text-gray-500 md:w-4/5">
+            {productData.description}
+          </p>
+          <div className="flex flex-col gap-4 my-8">
+            <p>Select Size</p>
+            <div className="flex gap-2">
+              {productData.sizes.map((item, index) => (
+                <button
+                  onClick={() => setSize(item)}
+                  className={`border py-2 px-4 bg-gray-100 ${
+                    item === size ? "border-orange-500" : ""
+                  }`}
+                  key={index}
+                >
+                  {item}
+                </button>
+              ))}
+            </div>
+          </div>
+          <button onClick={()=>addToCart(productData._id,size)} className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700 uppercase">
+            Add to cart
+          </button>
+          <hr className="mt-8 sm:w-4/5" />
+          <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1">
+            <p>100% Original product.</p>
+            <p>CAsh on delivery is availabel on this product.</p>
+            <p>Easy return and exchange policy within 7 days.</p>
+          </div>
+        </div>
       </div>
+      {/* Description & Review section*/}
+      <div className="mt-20">
+        <div className="flex">
+          <b className="border px-5 py-3 text-sm"> Description</b>
+          <p className="border px-5 py-3 text-sm">Reviews (122) </p>
+        </div>
+        <div className="flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500">
+          <p>
+            E-commerce is the activity of electronically buying or selling
+            products on online services or over the Internet. E-commerce draws
+            on technologies such as mobile commerce, electronic funds transfer,
+            supply chain management, Internet marketing, online transaction
+            processing, electronic data interchange, inventory management
+            systems, and automated data collection systems. E-commerce is the
+            largest sector of the electronics industry and is in turn driven by
+            the technological advances of the semiconductor industry.
+          </p>
+          <p>
+            Marketing texts for regular pages: a home page, About Us, payment
+            options, guarantees, FAQs, and contacts
+          </p>
+        </div>
+      </div>
+
+      {/* Display Related Products */}
+      <RelatedProducts category={productData.category} subCategory={productData.subCategory}/>
     </div>
   ) : (
     <div className="opacity-0"></div>
